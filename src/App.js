8c4c3b3f56f7b00115bfda61cd0625a2ae3d9c31
salmansiddiqui <salman079@gmail.com>
2020-08-20 22:05:41 +0300

    class4

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {Message} from './message.js'
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(0) 
+  let [num, setNum] = useState(0) 
+  let [isMorning, setisMorning] = useState(false) 
+  
+  // setCount(+2);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`box ${isMorning ? 'daylight' : ''}`}>
+      <h1>Day time = {isMorning ? 'Morning' : 'Night'} </h1>
+      <button onClick={()=> setisMorning(!isMorning)}> Change dayLight </button><br /><br />
+      
+      <Message counter={count} />
+      {count}
+      <br />
+      <button onClick={()=> alert('Button pressed, '+count+' counts')}> update counter for alert!!!!</button> <br /><br />
+      <button onClick={()=> setCount(count+1)}> Add counter </button><br /><br />
+      <button onClick={()=> setCount(count-1)}> Sub counter </button><br />
+      <label>Enter Number:
+                <input type="text" name="num" size="25" maxlength="40" /><br />
+      </label>
+      <button onClick={()=> setNum(num+1)}> Add counter </button><br /><br />
+      <button onClick={()=> setNum(num-1)}> Sub counter </button><br />
+      {num}
+      
+      <br />
+      
     </div>
-  );
+
+    );
 }
 
 export default App;
---
+++ src/message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function Message (props){
+    return (
+        <h3>Value of counte variable through message function {props.counter}</h3>
+    )
+}
